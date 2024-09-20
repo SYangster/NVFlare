b08707d3c944ed6ceefdec0f81e8cf88968d15b5
Sean Yang <seany314@gmail.com>
2024-09-20 14:26:17 -0700

    add web versioning

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+const branch = process.env.PUBLIC_GH_BRANCH;
+
 export default defineConfig({
-  site: "https://nvidia.github.io",
-  base: "/NVFlare",
+  site: "https://syangster.github.io",
+  base: branch === 'main' ? '/NVFlare' : `/NVFlare/version/${branch}`,
   integrations: [tailwind()],
 });
