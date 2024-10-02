1aa6ed63ecffb581901efec2c4e5148a1dc3dc3a
Sean Yang <seany314@gmail.com>
2024-10-02 10:54:21 -0700

    add simple analytics

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from "@astrojs/tailwind";
 const branch = process.env.PUBLIC_GH_BRANCH;
 
 export default defineConfig({
-  site: "https://nvidia.github.io",
+  site: "https://syangster.github.io",
   base: branch === 'main' ? '/NVFlare' : `/NVFlare/version/${branch}`,
   integrations: [tailwind()],
 });
